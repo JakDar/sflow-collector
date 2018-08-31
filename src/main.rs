a501027abd0b7fd548dad9ba84a6851714b59b40
Jakub Darul <daruljakub@gmail.com>
2018-08-31 07:28:55 +0200

    initial changes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 extern crate sflow;
+
 use std::net::UdpSocket;
 use std::io::BufReader;
 use std::io::Cursor;
 use sflow::Decodeable;
 
 
+fn print_flow_record(record: &sflow::FlowRecord) {
+    use sflow::FlowRecord::*;
+    match record {
+        SampledHeader(sample) => println!("Sample: {:?}", sample),
+        SampledIpv4(sample) => println!("Sample: {:?}", sample),
+        _ => ()
+    }
+}
+
 fn main() {
-    let mut stream = UdpSocket::bind("0.0.0.0:1337").unwrap();
-    
+    let mut stream = UdpSocket::bind("0.0.0.0:6343").unwrap();
+
     let mut buffer = [0u8; 1500];
 
     loop {
-        let mut packet : &mut [u8] = &mut buffer;
+        let mut packet: &mut [u8] = &mut buffer;
         let (size, remote) = match stream.recv_from(packet) {
             Err(e) => {
                 println!("Failed to read from socket: {:?}", e);
@@ -28,15 +38,18 @@ fn main() {
         }
 
         let mut cur = Cursor::new(packet);
-        let dgram = match sflow::Datagram::read_and_decode(&mut cur) {
+        let dgram: sflow::Datagram = match sflow::Datagram::read_and_decode(&mut cur) {
             Err(e) => {
                 println!("failed to decode sample: {:?}", e);
-                continue
+                continue;
             }
             Ok(some) => some
         };
         for sample in &dgram.sample_record {
-            println!("Sample: {:?}", sample);
+            match sample {
+                sflow::SampleRecord::FlowSample(flow) => flow.flow_records.iter().for_each(|record| print_flow_record(record)),
+                _ => ()
+            }
         }
     }
 }
